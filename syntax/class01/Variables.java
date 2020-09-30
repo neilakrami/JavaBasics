b916364a5f2f89362ba812b46cd6f7eb264e49f1
VYOM GUPTA <55016130+vyom104@users.noreply.github.com>
2020-10-01 00:41:49 +0530

    Update Variables.java

--- syntax/class01/Variables.java
+++ syntax/class01/Variables.java
@@ -4,10 +4,13 @@ public class Variables {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+	//Assigning 10 to variableone
 	byte variableone = 10;
+	// Printing Value of variableone
 	System.out.println(variableone);
+	// Printing String variableone
 	System.out.println("variableone");
+	// Printing 10
 	System.out.println(10);
 	}
 
